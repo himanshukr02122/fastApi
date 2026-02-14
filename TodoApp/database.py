e4540805ba3a025bb67b7b0a8fc1dc17431b4ab5
himanshukr02122 <himanshukumar02122@gmail.com>
2026-02-14 22:29:49 +0530

    using sqlite

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Him%400646@127.0.0.1:3306/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
